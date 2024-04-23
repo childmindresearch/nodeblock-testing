d8c2311f4f704ad58533bc8477b1644634fbd8d7
Jon Clucas <jon.clucas@childmind.org>
2024-04-23 13:33:44 -0400

    :coffin: Remove (unused) `ResourcePoolPools`

--- src/cpac_nodeblock_testing/resource_pools/__init__.py
+++ src/cpac_nodeblock_testing/resource_pools/__init__.py
@@ -15,7 +15,3 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with C-PAC. If not, see <https://www.gnu.org/licenses/>.
 """ResourcePools."""
-
-from cpac_nodeblock_testing.resource_pools.resource_pool_pool import ResourcePoolPool
-
-__all__ = ["ResourcePoolPool"]
